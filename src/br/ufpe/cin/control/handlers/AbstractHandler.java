6363acc88c3e538d45942ccbb30a88272247c8ff
ViniciusOliveiraAndrade <viniciusibsm@gmail.com>
2020-08-29 11:12:21 -0300

    Added the AdicionarAcacaoWindow and AdicionarVariavelWindow variables to the AbstracHandlers

--- src/br/ufpe/cin/control/handlers/AbstractHandler.java
+++ src/br/ufpe/cin/control/handlers/AbstractHandler.java
@@ -3,12 +3,19 @@ package br.ufpe.cin.control.handlers;
 import java.awt.event.ActionListener;
 import java.util.Observer;
 
+import br.ufpe.cin.view.AdicionarAcaoWindow;
+import br.ufpe.cin.view.AdicionarVariavelWindow;
 import br.ufpe.cin.view.GerenciadoTransacaoPanel;
 
 public abstract class AbstractHandler implements ActionListener, Observer {
 	
 	private GerenciadoTransacaoPanel gtp;
-
+	
+	private AdicionarAcaoWindow aaw;
+	
+	private AdicionarVariavelWindow avw;
+	
+	
 	public AbstractHandler(GerenciadoTransacaoPanel gtp) {
 		this.gtp = gtp;
 	}
@@ -19,6 +26,22 @@ public abstract class AbstractHandler implements ActionListener, Observer {
 
 	public void setGtp(GerenciadoTransacaoPanel gtp) {
 		this.gtp = gtp;
+	}
+
+	public AdicionarAcaoWindow getAaw() {
+		return aaw;
+	}
+
+	public void setAaw(AdicionarAcaoWindow aaw) {
+		this.aaw = aaw;
+	}
+
+	public AdicionarVariavelWindow getAvw() {
+		return avw;
+	}
+
+	public void setAvw(AdicionarVariavelWindow avw) {
+		this.avw = avw;
 	}
 	
 	
