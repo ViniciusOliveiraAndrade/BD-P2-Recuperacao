8f76055850a7547b3dbe9c82bc7afa6a8da3a910
ViniciusOliveiraAndrade <viniciusibsm@gmail.com>
2020-08-31 14:27:54 -0300

    melhorei a tela

--- src/br/ufpe/cin/view/EventoHolder.java
+++ src/br/ufpe/cin/view/EventoHolder.java
@@ -1,6 +1,7 @@
 package br.ufpe.cin.view;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -18,39 +19,39 @@ public class EventoHolder extends JLabel{
 		this.setOpaque(true);
 		switch (evento.getTipo()) {
 		case "TRANSACAO":
-			this.setText("[ START_TRANSACTION, T"+evento.getTransacao().getCod()+" ]");
+			this.setText(" [ START_TRANSACTION, T"+evento.getTransacao().getCod()+" ]");
 			break;
 		case "ACAO":
 			if (evento.getAcao().getTipo() == StringVariables.ACAO_WRITE.getValue()) {
-				this.setText("[ WRITE_ITEM, T"+evento.getTransacao().getCod() + ", "+evento.getAcao().getVariavelAlvo().getNome() + ", " +evento.getAcao().getValorVelho() + ", " +evento.getAcao().getValorNovo()+" ]");
+				this.setText(" [ WRITE_ITEM, T"+evento.getTransacao().getCod() + ", "+evento.getAcao().getVariavelAlvo().getNome() + ", " +evento.getAcao().getValorVelho() + ", " +evento.getAcao().getValorNovo()+" ]");
 			}else if (evento.getAcao().getTipo() == StringVariables.ACAO_READ.getValue()) {
-				this.setText("[ READ_ITEM, T"+evento.getTransacao().getCod() + ", "+evento.getAcao().getVariavelAlvo().getNome()+" ]");
+				this.setText(" [ READ_ITEM, T"+evento.getTransacao().getCod() + ", "+evento.getAcao().getVariavelAlvo().getNome()+" ]");
 			}
 			break;
 		case "CHECKPOINT":
-			this.setText("[ CHECKPOINT ]");
+			this.setText(" [ CHECKPOINT ]");
 			break;
 		case "VARIAVEL":
-			this.setText(evento.getVariavel().getNome()+ " = " + evento.getVariavel().getValor());
+			this.setText(" "+evento.getVariavel().getNome()+ " = " + evento.getVariavel().getValor());
 			break;
 		case "ABORT":
-			this.setText("[ ABORT_TRANSACTION, T"+evento.getTransacao().getCod()+" ]");
+			this.setText(" [ ABORT_TRANSACTION, T"+evento.getTransacao().getCod()+" ]");
 			break;
 		case "COMMIT":
-			this.setText("[ COMMIT_TRANSACTION, T"+evento.getTransacao().getCod()+" ]");
+			this.setText(" [ COMMIT_TRANSACTION, T"+evento.getTransacao().getCod()+" ]");
 			break;
 		case "UNDO_REDO":
 			if(evento.getUNDO_REDO() == "UNDO") {
-				this.setText("[UNDO, T"+evento.getTransacao().getCod()+" ]");
+				this.setText(" [ UNDO, T"+evento.getTransacao().getCod()+" ]");
 			}else {
-				this.setText("[REDO, T"+evento.getTransacao().getCod()+" ]");
+				this.setText(" [ REDO, T"+evento.getTransacao().getCod()+" ]");
 			}
 			break;
 			
 		default:
 			break;
 		}
-		
+		this.setFont(new Font(this.getFont().getFamily(), 0, 20));
 	}
 
 }
